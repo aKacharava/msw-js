99d7e50e9837c0b1701bf5a5bbc7f1e8960d8716
alexander <alexander@muze.nl>
2022-03-07 11:22:57 +0100

    Implemented dry code of oAuth2Fetch, FIXME: Oauth2Fetch was blocked because of a disallowed MIME type

---
+++ src/DefaultFetch.js
@@ -0,0 +1,9 @@
+export const defaultFetch = function (req) {
+  return fetch(req).then((res) => {
+    if (!res.ok) {
+      throw new FetchError(res.status + ": " + res.statusText, { cause: res });
+    }
+  });
+};
+
+export const FetchError = class FetchError extends NetworkError {};
--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
 // Import MSW from CDN
 const msw = window.MockServiceWorker;
 
+import { oauth2Fetch } from "./OAuth2Fetch";
+
 // This configures a Service Worker with the given request handlers.
 const worker = msw.setupWorker(
-  msw.rest.get(
-    "https://authorize-server.mock/authorize",
-    (req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          code: "authorizeToken"
-        })
-      );
-    }
-  ),
-  msw.rest.get("https://authorize-server.mock/token", (req, res, ctx) => {})
+  msw.rest.get( "https://authorize-server.mock/authorize", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        code: "authorizeToken"
+      })
+    );
+  }),
+  msw.rest.get("https://authorize-server.mock/token", (req, res, ctx) => {
+
+  })
 );
 
 worker.start({
@@ -23,14 +24,8 @@ worker.start({
   },
 });
 
-function auth(){
-  fetch(
-    "https://authorize-server.mock/authorize"
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      alert(data.token);
-    });
+async function auth(){
+  oauth2Fetch("https://authorize-server.mock/authorize");
 }
+
+document.querySelector("button").onclick = auth();
